01f3c1403c6500cca94a6038cc20686f5bf85191
Tyler Cash <contact@tylercash.xyz>
2017-02-26 01:21:46 +1100

    Created house object to store important information of house

---
+++ house.py
@@ -0,0 +1,15 @@
+class House(object):
+    address = ""
+    cost_per_week = 0
+    url = ""
+
+    # The class "constructor" - It's actually an initializer
+    def __init__(self, address, cost_per_week, url):
+        self.address = address
+        self.cost_per_week = cost_per_week
+        self.url = url
+
+
+def create_house(address, cost_per_week, url):
+    house = House(address, cost_per_week, url)
+    return house
